abf6f94ef709164113a97b02a5f9aa6dd7c49be4
Caleb Johnson <caleb.johnson144@gmail.com>
2017-08-02 00:34:54 -0500

    I tried to play around with creating a first model with rover_simulationA. I pretty much added 5 boxes (1 as the frame and 4 as wheels).

--- rover_simulationA.cpp
+++ rover_simulationA.cpp
@@ -49,7 +49,7 @@ int main(int argc, char* argv[]) {
     // Easy shortcuts to add camera, lights, logo and sky in Irrlicht scene:
 	
     application.AddTypicalLights();
-    application.AddTypicalCamera(core::vector3df(2, 2, -5),
+    application.AddTypicalCamera(core::vector3df(10, 6, -15),
                                  core::vector3df(0, 1, 0));  // to change the position of camera
     // application.AddLightWithShadow(vector3df(1,25,-5), vector3df(0,0,0), 35, 0.2,35, 55, 512, video::SColorf(1,1,1));
 
@@ -60,17 +60,27 @@ int main(int argc, char* argv[]) {
     // An example: a pendulum.
 
     // 1-Create a floor that is fixed (that is used also to represent the absolute reference)
+	
+	// Create a material that will be used for the floor
+	auto mmaterial = std::make_shared<ChMaterialSurfaceNSC>();
+	//mmaterial->SetFriction(0.4f);
+	//mmaterial->SetCompliance(0.0000005f);
+	//mmaterial->SetComplianceT(0.0000005f);
+	//mmaterial->SetDampingF(0.2f);
 
-    auto floorBody = std::make_shared<ChBodyEasyBox>(10, 2, 10,  // x, y, z dimensions
-                                                     3000,       // density
-                                                     false,      // no contact geometry
-                                                     false        // enable visualization geometry
+    auto floorBody = std::make_shared<ChBodyEasyBox>(30, 2, 30,  // x, y, z dimensions
+                                                     1000,       // density
+                                                     true,      // contact geometry - allow collision
+                                                     true        // enable visualization geometry
                                                      );
     floorBody->SetPos(ChVector<>(0, -2, 0));
+	floorBody->SetMaterialSurface(mmaterial);
     floorBody->SetBodyFixed(true);
 
     mphysicalSystem.Add(floorBody);
-
+	
+	
+	/* comment out pendulum for possible future use
     // 2-Create a pendulum
 
     auto pendulumBody = std::make_shared<ChBodyEasyBox>(0.5, 2, 0.5,  // x, y, z dimensions
@@ -107,6 +117,96 @@ int main(int argc, char* argv[]) {
     auto texture = std::make_shared<ChTexture>();
     texture->SetTextureFilename(GetChronoDataFile("rock.jpg"));  // texture in ../data
     pendulumBody->AddAsset(texture);
+	*/
+
+	/*attempt to make wheels (template from demo_IRR_aux_ref)
+	// Physical Rover body
+	auto rover = std::make_shared<ChBody>();
+	mphysicalSystem.AddBody(rover);
+	rover->SetIdentifier(1);
+	rover->SetBodyFixed(false);
+	rover->SetCollide(false);
+	rover->SetMass(1);
+
+	// Add Frame
+	auto frameBox = std::make_shared<ChBodyEasyBox>(10, 2, 15,	// x,y,z size
+		100,													// density
+		true,													// collide enable?
+		true													// visualization?		
+		);
+
+	rover->AddAsset(frameBox);
+
+	// Attach a visualization asset. Note that the cylinder is defined with
+	// respect to the centroidal reference frame (which is the body reference
+	// frame for a ChBody).
+	auto wheel_1 = std::make_shared<ChCylinderShape>();
+	wheel_1->GetCylinderGeometry().p1 = ChVector<>(-2, 0, 0);
+	wheel_1->GetCylinderGeometry().p2 = ChVector<>(5, 0, 0);
+	wheel_1->GetCylinderGeometry().rad = 3;
+	rover->AddAsset(wheel_1);
+	//auto col_1 = std::make_shared<ChColorAsset>();
+	//col_1->SetColor(ChColor(0.6f, 0, 0));
+	//rover->AddAsset(col_1);
+	
+	rover->SetPos(ChVector<>(0, 3, 0));
+	mphysicalSystem.Add(rover);
+	*/
+
+	// Add Frame
+	auto frameBox = std::make_shared<ChBodyEasyBox>(10, 2, 15,	// x,y,z size
+		100,													// density
+		true,													// collide enable?
+		true													// visualization?		
+		);													
+	
+	frameBox->SetPos(ChVector<>(0, 3, 0));
+	
+	mphysicalSystem.Add(frameBox);
+
+	// Add wheels
+	/* attempt to use cylinders
+	auto wheel_1 = std::make_shared<ChBodyEasyCylinder>(
+		3, // radius
+		2, // height
+		100,// density
+		true,// collide
+		true// visualization		
+		);
+	*/
+	auto wheel_1 = std::make_shared<ChBodyEasyBox>(2, 6, 6,	// x,y,z size
+		100,													// density
+		true,													// collide enable?
+		true													// visualization?		
+		);
+	wheel_1->SetPos(ChVector<>(6, 3, 6));
+	mphysicalSystem.Add(wheel_1);
+	
+	auto wheel_2 = std::make_shared<ChBodyEasyBox>(2, 6, 6,	// x,y,z size
+		100,													// density
+		true,													// collide enable?
+		true													// visualization?		
+		);
+	wheel_2->SetPos(ChVector<>(-6, 3, 6));
+	mphysicalSystem.Add(wheel_2);
+
+	auto wheel_3 = std::make_shared<ChBodyEasyBox>(2, 6, 6,	// x,y,z size
+		100,													// density
+		true,													// collide enable?
+		true													// visualization?		
+		);
+	wheel_3->SetPos(ChVector<>(6, 3, -6));
+	mphysicalSystem.Add(wheel_3);
+
+	auto wheel_4 = std::make_shared<ChBodyEasyBox>(2, 6, 6,	// x,y,z size
+		100,													// density
+		true,													// collide enable?
+		true													// visualization?		
+		);
+	wheel_4->SetPos(ChVector<>(-6, 3, -6));
+
+	mphysicalSystem.Add(wheel_4);
+
 
     //======================================================================
 
